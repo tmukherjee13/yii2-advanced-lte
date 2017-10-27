157a07206657c51e2c0c787ff5ef162e03228c9c
Tarun M <tarun.mukherjee@indusnet.co.in>
2017-10-27 18:40:12 +0530

    added layouts

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -11,14 +11,23 @@ $config = [
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][]      = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
     ];
 
-    $config['bootstrap'][] = 'gii';
+    $config['bootstrap'][]    = 'gii';
     $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+        'class'      => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.0.*'],
+        'generators' => [ //here
+            'crud' => [
+                'class'     => 'yii\gii\generators\crud\Generator',
+                'templates' => [
+                    'Custom' => '@vendor/dmstr/yii2-adminlte-asset/gii/templates/crud/simple',
+                ],
+            ],
+        ],
     ];
 }
 
